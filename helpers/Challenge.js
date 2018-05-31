fd3f4eeb86035facb9be5cc057bf0ed3fc0956b5
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2018-05-31 02:05:10 -0300

    Cleaning up

---
+++ helpers/Challenge.js
@@ -0,0 +1,43 @@
+"use strict";
+
+let firstOperand;
+let secondOperand;
+let result;
+let operator;
+const operators = {
+  1: "*",
+  2: "+",
+  3: "-",
+  4: "/"
+};
+
+class Challenge {
+  getRandomNumber(max) {
+    const min = 1;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  newChallenge() {
+    firstOperand = this.getRandomNumber(10);
+    secondOperand = this.getRandomNumber(10);
+    operator = operators[this.getRandomNumber(4)];
+    result = eval(firstOperand + operator + secondOperand);
+
+    switch (this.getRandomNumber(2)) {
+      case 1:
+        result = eval(result + operator + this.getRandomNumber(10));
+        break;
+      default:
+        break;
+    }
+
+    return {
+      firstOperand: firstOperand,
+      operator: operator,
+      secondOperand: secondOperand,
+      result: result
+    };
+  }
+}
+
+module.exports = new Challenge();
--- index.js
+++ index.js
@@ -1,70 +1,20 @@
-let express = require("express");
-let app = express();
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.Server(app);
+const socketIO = require("socket.io");
+const io = socketIO(server);
 
-let http = require("http");
-let server = http.Server(app);
-
-let socketIO = require("socket.io");
-let io = socketIO(server);
+const helper = require("./helpers/Challenge");
 
 const port = process.env.PORT || 3000;
 
 let connected = 0;
-// app.get("/connected", function(req, res) {
-//   res.json({ connected: connected });
-
-//   // res.sendFile(__dirname + "/index.html");
-// });
-
-let firstOperand;
-let secondOperand;
-let result;
-let operator;
-const operators = {
-  1: "*",
-  2: "+",
-  3: "-",
-  4: "/"
-};
-
-function getRandomNumber(max) {
-  const min = 1;
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function newChallenge() {
-  firstOperand = getRandomNumber(10);
-  secondOperand = getRandomNumber(10);
-  operator = operators[getRandomNumber(4)];
-  result = eval(firstOperand + operator + secondOperand);
-
-  switch (getRandomNumber(2)) {
-    case 1:
-      result = eval(result + operator + getRandomNumber(10));
-      break;
-    default:
-      break;
-  }
-
-  return {
-    firstOperand: firstOperand,
-    operator: operator,
-    secondOperand: secondOperand,
-    result: result
-  };
-}
-
-console.log(newChallenge());
-// console.log(`${firstOperand} ${operator} ${secondOperand} = ${result}`);
 
 io.on("connection", socket => {
   io.emit("connected", io.clients().server.engine.clientsCount);
-  io.emit("challenge", {
-    firstOperand: firstOperand,
-    operator: operator,
-    secondOperand: secondOperand,
-    result: result
-  });
+
+  io.emit("challenge", helper.newChallenge());
 
   socket.on("answer", data => {
     io.emit("answered", data);
@@ -74,12 +24,8 @@ io.on("connection", socket => {
     io.emit("connected", io.clients().server.engine.clientsCount);
   });
 
-  socket.on("new-message", message => {
-    console.log(message);
-  });
   socket.on("new-challenge", data => {
-    console.log(data);
-    io.emit("challenge", newChallenge());
+    io.emit("challenge", helper.newChallenge());
   });
 });
 
